2ceb79781e5edf6c49a952b2934206c3f6e0751a
Stephanie <percheron.stephanie@gmail.com>
2019-10-03 19:07:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let modal = null
+
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display = null
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()
+    window.setTimeout(function () {
+        modal.style.display = "none"
+        modal = null
+    }, 500)
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal.close').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removesEventListener('click', stopPropagation)
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)
+})
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+})
